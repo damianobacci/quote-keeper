fcec1eeac663c7612d7cae3db37a99f409e848ca
Damiano <damiano.bacci@gmail.com>
2023-05-24 16:11:26 +0200

    Testing by saving the content to an object

--- testing/script.js
+++ testing/script.js
@@ -1,9 +1,20 @@
+let storage = {};
+
 function logSelectedText() {
   let text = "";
   if (window.getSelection) {
     text = window.getSelection().toString();
   }
-  console.log(text);
-}
+  let author = prompt("Please enter the author's name");
+  let tag = prompt("Please enter a tag");
 
+  if (author !== null) {
+    storage = {
+      author: author,
+      text: text,
+      tag: tag,
+    };
+  }
+  console.log(storage);
+}
 document.querySelector("#logger").addEventListener("click", logSelectedText);
